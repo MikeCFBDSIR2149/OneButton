3880c9e92fde7dbfc8d6d5e642f06464421a8e11
MikeCFBDSIR2149 <mike_cfbdsir2149@outlook.com>
2025-02-26 15:58:00 +0800

    初次提交

---
+++ Assets/FunkyCode/Demos - SmartSlicer2D/Scripts/Rope2D.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope2D : MonoBehaviour {
+	public GameObject anchorBody;
+	public GameObject connectedBody;
+
+	public Color color;
+
+	private List<GameObject> nodes = new List<GameObject> ();
+
+	void Start () {
+		Vector2f position = new Vector2f(anchorBody.transform.position);
+
+		GameObject prev = anchorBody;
+
+		int ropeId = 1;
+		GameObject gObject = null;
+		while (Vector2f.Distance (position, new Vector2f(connectedBody.transform.position)) > .5f) {
+			float direction = Vector2f.Atan2 (new Vector2f(connectedBody.transform.position), position);
+
+			gObject = new GameObject();
+			gObject.AddComponent<HingeJoint2D>().connectedBody = prev.GetComponent<Rigidbody2D>();
+
+			gObject.transform.parent = transform;
+			gObject.transform.position = position.Get ();
+			gObject.name = "Rope " + ropeId;
+			gObject.AddComponent<JointRenderer2D> ().color = color;
+			gObject.AddComponent<CircleCollider2D> ().radius = .25f;
+
+			nodes.Add(gObject);
+
+			position.Push (direction, .5f);
+
+			prev = gObject;
+			ropeId++;
+		}
+
+		if (gameObject != null)
+			gObject.AddComponent<HingeJoint2D>().connectedBody = connectedBody.GetComponent<Rigidbody2D>();
+
+	}
+}
